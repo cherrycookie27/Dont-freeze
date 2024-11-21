34e1fbb68478683f75a2dd442c613648e2ca9848
cherrycookie27 <jussilatara@gmail.com>
2024-11-21 09:51:48 +0200

    Puspus

--- Assets/Scripts/Zommby.cs
+++ Assets/Scripts/Zommby.cs
@@ -1,17 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Zommby : MonoBehaviour
 {
-    private bool isAttacking = false; 
+    public GameObject player;
     public Animator anim;
+    public float speed;
+
+    private Rigidbody2D rb;
+    private bool isAttacking = false;
+    private float distance;
 
     private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
 
+    private void FixedUpdate()
+    {
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+
+        if (distance < 22)
+        {
+                transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+                anim.SetBool("IsWalking", true);
+        }
+
+        if (distance > 22)
+        {
+                anim.SetBool("IsWalking", false);
+        }
+
+        else
+        {
+            anim.SetBool("IsWalking", false);
+        }
+    }
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player") && !isAttacking)
